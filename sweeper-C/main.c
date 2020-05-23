26da7a2d1d17c5da3927c52c4cd9da024daf9548
KEVIN <43997638+china-Yyyang@users.noreply.github.com>
2020-05-23 23:07:55 +0800

    Create main.c

---
+++ sweeper-C/main.c
@@ -0,0 +1,46 @@
+#include"game.h"
+ 
+void menu()
+{
+    printf("*****************************************\n");
+    printf("***************1.开始游戏****************\n");
+    printf("************   0.退出游戏   *************\n");
+    printf("*****************************************\n");
+}
+ 
+void game()
+{
+    char mine[ROW][COL];
+    char show[ROW][COL];
+    init_board(mine, show);
+    display_board(show);
+    set_mine(mine);
+//    show_board(mine);
+    my_step(mine,show);
+}
+ 
+int main()
+{
+    int input=0;
+    do
+    {
+        menu();
+        printf("请选择\n");
+        scanf("%d",&input);
+        switch(input)
+        {
+        case 1:
+            printf("请开始游戏\n");
+            game();
+            break;
+        case 0:
+            printf("退出游戏\n");
+            break;
+        default:
+            printf("请重新选择\n");
+            break;
+        }
+    }
+    while(input);
+    return 0;
+}
